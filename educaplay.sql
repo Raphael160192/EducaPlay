0bff47aeba0e1b8f085598cc6d9f0fccb44f1e58
edivaldogundim <edivaldogundim@gmail.com>
2021-02-15 20:13:01 -0300

    update backend

--- educaplay.sql
+++ educaplay.sql
@@ -1,5 +1,3 @@
-create schema educaplay;
-
 CREATE TABLE `usuario` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(30) DEFAULT NULL,
@@ -13,26 +11,13 @@ CREATE TABLE `usuario` (
   `senha` char(32),
   `confirmaSenha` char(32),
   PRIMARY KEY (`id`)
-);
+)
 
-<<<<<<< HEAD
-create table materias(
-idmateria int not null auto_increment,
-disciplina varchar(50) not null,
-professor varchar(50),
-primary key(idmateria)
-);
-
-=======
->>>>>>> a319069774855b73320cccd950adaa57af77c20c
 CREATE TABLE `comentario` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(30) DEFAULT NULL,
   `msg` varchar(400) NOT NULL,
   PRIMARY KEY (`id`)
-<<<<<<< HEAD
-);
-=======
 )
 
->>>>>>> a319069774855b73320cccd950adaa57af77c20c
+
